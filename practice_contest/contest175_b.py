aa2b963f4b6ed00bba233e1a4b0c5a23ca421128
moffy-Black <58592807+moffy-Black@users.noreply.github.com>
2020-08-20 12:44:18 +0900

    understand content175_b

--- practice_contest/contest175_b.py
+++ practice_contest/contest175_b.py
@@ -1,17 +1,24 @@
 # -*- using:utf-8 -*-
+from itertools import combinations
 
 class main:
   def T(array):
-    s = 0
+    cnt = 0
+    for i in array:
+      a, b, c = i
+      if a != b and a != c and b != c and a < b + c:
+        cnt += 1
+    print(cnt)
+
+  def Sort(array):
     for i in range(len(array)):
-      i += 1
-      for j in range(len(array) - (i - 1)):
-        s+=1
-        print(array[j], array[j + 1], array[j + 2])
-        
+      array[i] = sorted(array[i], reverse=True)
+    return array
+
 if __name__ == "__main__":
   n = int(input())
   l = list(map(int, input().split()))
-  array = list(set(l))
+  array = list(combinations(l, 3))
+  array = main.Sort(array)
   main.T(array)
       
