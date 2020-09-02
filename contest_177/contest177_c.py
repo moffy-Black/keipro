5eddfb61652dfcfd547955ae958e3e7988224387
moffy-Black <58592807+moffy-Black@users.noreply.github.com>
2020-09-02 22:51:29 +0900

    fix contest177_c

--- contest_177/contest177_c.py
+++ contest_177/contest177_c.py
@@ -1,13 +1,40 @@
 # -*- using:utf-8 -*-
-from itertools import combinations
+
+class main:
+  def function(n, a):
+    mod = 10 **9 +7
+    S = [0] * (n + 1)
+    for i in range(n):
+      S[i + 1] = S[i] + a[i]
+    SN = S[-1]
+    ans = 0
+    for i in range(n - 1):
+      cnt = a[i] * (SN - S[i + 1])
+      ans += cnt
+    print(ans % mod)
+
+
 
 if __name__ == "__main__":
   n = int(input())
-  li = list(map(int, input().split()))
-  array = list(combinations(li, 2))
-  sum = 0
-  mod = 1000000007
-  for i in range(n):
-    sum += array[i][0] * array[i][1]
-  ans = sum % mod
-  print(ans)
+  a = list(map(int, input().split()))
+  main.function(n,a)
+
+
+
+# self------------------------------------
+# from itertools import combinations
+
+# class main:
+#   def v(array):
+#     sum = 0
+#     for i in range(len(array)):
+#       sum += array[i][0] * array[i][1]
+#     sum = sum % 1000000007
+#     print(sum)
+
+# if __name__ == "__main__":
+#   n = int(input())
+#   a = list(map(int, input().split()))
+#   array = list(combinations(li, 2))
+#   main.v(array)
